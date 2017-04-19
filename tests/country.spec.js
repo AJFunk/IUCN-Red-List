af375db1c25fdfc3abd36a4cce9b16909f61420e
AJ Funk <aj.w.funk.9@gmail.com>
2017-04-18 20:06:00 -0700

    add tests for Country module

---
+++ tests/country.spec.js
@@ -0,0 +1,23 @@
+import { Country } from 'index';
+
+describe('Country.all()', () => {
+  it('returns object with results array', () =>
+    Country.all().then(data => {
+      expect(data).to.be.instanceof(Object);
+      expect(data.results).to.be.instanceof(Array);
+    })
+  );
+});
+
+describe('Country.species()', () => {
+  it('returns object with results array', () =>
+    Country.species('uz').then(data => {
+      expect(data).to.be.instanceof(Object);
+      expect(data.result).to.be.instanceof(Array);
+    })
+  );
+
+  it('returns error message', () =>
+    Country.species().catch(err => expect(err).to.be.an('error'))
+  );
+});
